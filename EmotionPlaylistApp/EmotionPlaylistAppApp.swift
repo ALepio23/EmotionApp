75462bb21c1eefd71e1aba9a5e8c00fe56215fc9
Alessandro <alessandro.alvigi23@gmail.com>
2024-12-12 10:09:53 +0100

    Initial Commit

---
+++ EmotionPlaylistApp/EmotionPlaylistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionPlaylistAppApp.swift
+//  EmotionPlaylistApp
+//
+//  Created by Alessandro Pio Alvigi on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EmotionPlaylistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
